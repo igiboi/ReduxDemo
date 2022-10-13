38a99dd15203b0185d52f8c1554942ca997c914b
Luigi Matias <luigi.matias@codehunter.com>
2022-10-12 21:38:15 -0400

    added a payload

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
  
 const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === "increment")
+    if (action.type === "increment") {
         return {
             counter: state.counter + 1,
         };
-    else if (action.type === "decrement")
+    }
+
+    if (action.type === "increase") {
+        return {
+          counter: state.counter + action.amount,
+        };
+    }
+
+    else if (action.type === "decrement") {
         return {
             counter: state.counter - 1,
         };
- 
+    }
+    
     return state;
 };
  
